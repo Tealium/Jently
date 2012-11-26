5f54fbf9d3c4b6beb6a41422415415204dc49bca
Jen Frisk <jennifer.mitchell@tealium.com>
2012-11-26 15:17:28 -0800

    added admin user to git commands

--- lib/git.rb
+++ lib/git.rb
@@ -3,13 +3,19 @@ require 'systemu'
 require './lib/helpers.rb'
 
 module Git
+  def Git.admin
+    admin = "sudo -u jenkins"
+    return admin
+  end
+
   def Git.clone_repository
     config = ConfigFile.read
+    admin = Git.admin
     repository_id = Repository.get_id
     repository_dir = Repository.get_dir
     cmd = <<-GIT
       cd #{repository_dir} &&
-      git clone https://#{config[:github_login]}:#{config[:github_password]}@github.com/#{repository_id}.git
+      #{admin} git clone https://#{config[:github_login]}:#{config[:github_password]}@github.com/#{repository_id}.git
     GIT
     puts 'Cloning repository ...'
     status, stdout, stderr = systemu(cmd)
@@ -18,13 +24,14 @@ module Git
 
   def Git.delete_local_testing_branch
     config = ConfigFile.read
+    admin = Git.admin
     repository_path = Repository.get_path
     cmd = <<-GIT
       cd #{repository_path} &&
-      git reset --hard &&
-      git clean -df &&
-      git checkout master &&
-      git branch -D #{config[:testing_branch_name]}
+      #{admin} git reset --hard &&
+      #{admin} git clean -df &&
+      #{admin} git checkout master &&
+      #{admin} git branch -D #{config[:testing_branch_name]}
     GIT
     status, stdout, stderr = systemu(cmd)
     Logger.log("Deleting local testing branch - status: #{status} - stdout: #{stdout} - stderr: #{stderr}")
@@ -32,13 +39,14 @@ module Git
 
   def Git.delete_remote_testing_branch
     config = ConfigFile.read
+    admin = Git.admin
     repository_path = Repository.get_path
     cmd = <<-GIT
       cd #{repository_path} &&
-      git reset --hard &&
-      git clean -df &&
-      git checkout master &&
-      git push origin :#{config[:testing_branch_name]}
+      #{admin} git reset --hard &&
+      #{admin} git clean -df &&
+      #{admin} git checkout master &&
+      #{admin} git push origin :#{config[:testing_branch_name]}
     GIT
     status, stdout, stderr = systemu(cmd)
     Logger.log("Deleting remote testing branch - status: #{status} - stdout: #{stdout} - stderr: #{stderr}")
@@ -46,17 +54,18 @@ module Git
 
   def Git.create_local_testing_branch(pull_request)
     config = ConfigFile.read
+    admin = Git.admin
     repository_path = Repository.get_path
     cmd = <<-GIT
       cd #{repository_path} &&
-      git reset --hard &&
-      git clean -df &&
-      git fetch --all &&
-      git checkout #{pull_request[:head_branch]} &&
-      git reset --hard origin/#{pull_request[:head_branch]} &&
-      git clean -df &&
-      git checkout -b #{config[:testing_branch_name]} &&
-      git pull origin #{pull_request[:base_branch]}
+      #{admin} git reset --hard &&
+      #{admin} git clean -df &&
+      #{admin} git fetch --all &&
+      #{admin} git checkout #{pull_request[:head_branch]} &&
+      #{admin} git reset --hard origin/#{pull_request[:head_branch]} &&
+      #{admin} git clean -df &&
+      #{admin} git checkout -b #{config[:testing_branch_name]} &&
+      #{admin} git pull origin #{pull_request[:base_branch]}
     GIT
     status, stdout, stderr = systemu(cmd)
     Logger.log("Creating local testing branch - status: #{status} - stdout: #{stdout} - stderr: #{stderr}")
@@ -64,13 +73,14 @@ module Git
 
   def Git.push_local_testing_branch_to_remote
     config = ConfigFile.read
+    admin = Git.admin
     repository_path = Repository.get_path
     cmd = <<-GIT
       cd #{repository_path} &&
-      git reset --hard &&
-      git clean -df &&
-      git checkout #{config[:testing_branch_name]} &&
-      git push origin #{config[:testing_branch_name]}
+      #{admin} git reset --hard &&
+      #{admin} git clean -df &&
+      #{admin} git checkout #{config[:testing_branch_name]} &&
+      #{admin} git push origin #{config[:testing_branch_name]}
     GIT
     status, stdout, stderr = systemu(cmd)
     Logger.log("Pushing local testing branch to remote - status: #{status} - stdout: #{stdout} - stderr: #{stderr}")
