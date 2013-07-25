1720eca8d21c9a2c6c0cffab3f241bde6093108d
JenRed12 <jennifer.frisk@tealium.com>
2013-07-25 11:02:26 -0700

    Github.rb did not get reverted correctly.

--- lib/github.rb
+++ lib/github.rb
@@ -1,12 +1,13 @@
 require 'rubygems'
 require 'octokit'
+require './lib/helpers.rb'
 
 module Github
   def Github.get_open_pull_requests_ids
     begin
-      client = Github.new_client
+      config = ConfigFile.read
       repository_id = Repository.get_id
-
+      client = Octokit::Client.new(:login => config[:github_login], :password => config[:github_password])
       open_pull_requests = client.pull_requests(repository_id, 'open')
       open_pull_requests_ids = open_pull_requests.collect { |pull_request| pull_request.number }
     rescue => e
@@ -18,9 +19,9 @@ module Github
 
   def Github.get_pull_request(pull_request_id)
     begin
-      client = Github.new_client
+      config = ConfigFile.read
       repository_id = Repository.get_id
-
+      client = Octokit::Client.new(:login => config[:github_login], :password => config[:github_password])
       pull_request = client.pull_request(repository_id, pull_request_id)
       statuses = client.statuses(repository_id, pull_request.head.sha)
 
@@ -32,11 +33,14 @@ module Github
       data[:head_sha] = pull_request.head.sha
       data[:head_url] = pull_request.head.repo.ssh_url
       data[:head_fork] = pull_request.head.repo.fork
+      data[:last_checked] = Time.now.strftime("%Y-%m-%d %H:%M")
       data[:status] = statuses.empty? ? 'undefined' : statuses.first.state
-      data[:base_branch] = pull_request.base.ref    
+
+      # Update base_sha separately. The pull_request call is
+      # not guaranteed to return the last sha of the base branch.
+      data[:base_branch] = pull_request.base.ref
       data[:base_sha] = client.commits(repository_id, data[:base_branch]).first.sha
       data[:base_fork] = pull_request.base.repo.fork
-      data[:last_checked] = Time.now.strftime("%Y-%m-%d %H:%M")
       data
     rescue => e
       Logger.log('Error when getting pull request', e)
@@ -59,8 +63,7 @@ module Github
 
         #if there is already a valid comment/user found no need to check the rest.
         break if is_comment_valid == true
-
-        #Check to see if the user who commented on the pull request is liseted in the /config/config.yaml file
+ #Check to see if the user who commented on the pull request is liseted in the /config/config.yaml file
        has_correct_user = false
        has_correct_comment = false
         config[:testers].each do |tester, values|
@@ -92,6 +95,7 @@ module Github
 
   def Github.set_pull_request_status(pull_request_id, state, job_id=0, jenkins_job_name="")
     begin
+      config = ConfigFile.read
       repository_id = Repository.get_id
       head_sha = PullRequestsData.read[pull_request_id][:head_sha]
 
@@ -99,22 +103,16 @@ module Github
       opts[:target_url] = state[:url] if !state[:url].nil?
       opts[:description] = state[:description] if !state[:description].nil?
 
-      client = Github.new_client
+      client = Octokit::Client.new(:login => config[:github_login], :password => config[:github_password])
       client.create_status(repository_id, head_sha, state[:status], opts)
       Github.set_pull_request_comment(pull_request_id,state[:status], job_id, jenkins_job_name) if state[:status] !=  'pending'
-
-      PullRequestsData.update_status(pull_request_id, state[:status])
-
-      if state[:status] == 'success' || state[:status] == 'failure'
-        PullRequestsData.reset(pull_request_id)
-      end
     rescue => e
       Logger.log('Error when setting pull request status', e)
       sleep 5
       retry
     end
   end
-  
+
   def Github.set_pull_request_comment(pull_request_id, state_status, job_id, jenkins_job_name)
     begin
       config = ConfigFile.read
@@ -124,7 +122,7 @@ module Github
       else
         jenkins_status = Jenkins.get_job_state(job_id, jenkins_job_name)
         build_id = jenkins_status[:url].split('/').last
-        comment = "Jenkins build has completed with a status of #{state_status}. [Build # #{build_id}](#{jenkins_status[:url]})" 
+        comment = "Jenkins build has completed with a status of #{state_status}. [Build # #{build_id}](#{jenkins_status[:url]})"
       end
       client = Octokit::Client.new(:login => config[:github_login], :password => config[:github_password])
       client.add_comment(repository_id, pull_request_id, comment)
@@ -133,22 +131,5 @@ module Github
       sleep 5
       retry
     end
-  end
-
-  def Github.new_client
-    config = ConfigFile.read
-    if config.has_key?(:github_api_endpoint)
-      Octokit.configure do |c|
-        c.api_endpoint = config[:github_api_endpoint]
-        c.web_endpoint = config[:github_web_endpoint]
-      end
-    end
-
-    if config.has_key?(:github_oauth_token)
-      client = Octokit::Client.new(:login => config[:github_login], :oauth_token => config[:github_oauth_token])
-    else
-      client = Octokit::Client.new(:login => config[:github_login], :password => config[:github_password])
-    end
-    client
   end
 end
