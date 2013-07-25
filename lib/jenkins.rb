bf4393bb493c319ad145e9d02346f8018d8d7701
JenRed12 <jennifer.frisk@tealium.com>
2013-07-24 17:18:43 -0700

    revert merge

--- lib/jenkins.rb
+++ lib/jenkins.rb
@@ -1,11 +1,12 @@
+require 'rubygems'
 require 'faraday'
-require 'faraday_middleware'
+require './lib/helpers.rb'
 
 module Jenkins
   def Jenkins.wait_for_idle_executor
     config = ConfigFile.read
     while true
-      return if get_nb_of_idle_executors >= 1
+      return if Jenkins.get_nb_of_idle_executors >= 1
       sleep config[:jenkins_polling_interval_seconds]
     end
   end
@@ -13,7 +14,17 @@ module Jenkins
   def Jenkins.get_nb_of_idle_executors
     begin
       config = ConfigFile.read
-      connection = Jenkins.new_connection("#{config[:jenkins_url]}/api/json", config, :use_json => true)
+      connection = Faraday.new(:url => "#{config[:jenkins_url]}/api/json") do |c|
+        c.use Faraday::Request::UrlEncoded
+        c.use FaradayMiddleware::FollowRedirects
+        c.use FaradayMiddleware::Mashify
+        c.use FaradayMiddleware::ParseJson
+        c.use Faraday::Adapter::NetHttp
+      end
+
+      if config.has_key?(:jenkins_login) && config.has_key?(:jenkins_password)
+        connection.basic_auth config[:jenkins_login], config[:jenkins_password]
+      end
 
       response = connection.get do |req|
         req.params[:depth] = 1
@@ -27,21 +38,27 @@ module Jenkins
     end
   end
 
-  def Jenkins.new_job_id(pull_request_id)
-    "#{pull_request_id}-#{(Time.now.to_f * 1000000).to_i}"
-  end
-
-  def Jenkins.start_job(pull_request_id)
+  def Jenkins.start_job(jenkins_job_name, pull_request_id)
     begin
       config = ConfigFile.read
-      connection = Jenkins.new_connection("#{config[:jenkins_url]}/job/#{config[:jenkins_job_name]}/buildWithParameters", config)
-
-      job_id = new_job_id(pull_request_id)
-      connection.post do |req|
-        req.params[:id] = job_id
-        req.params[:branch] = config[:testing_branch_name]
-        req.params[:repository] = config[:github_ssh_repository]
+      connection = Faraday.new(:url => "#{config[:jenkins_url]}/job/#{jenkins_job_name}/buildWithParameters") do |c|
+        c.use Faraday::Request::UrlEncoded
+        c.use FaradayMiddleware::FollowRedirects
+        c.use Faraday::Adapter::NetHttp
       end
+
+      if config.has_key?(:jenkins_login) && config.has_key?(:jenkins_password)
+        connection.basic_auth config[:jenkins_login], config[:jenkins_password]
+      end
+
+      job_id = (Time.now.to_f * 1000000).to_i.to_s
+      connection.get do |req|
+        req.params[:id]           = job_id
+        req.params[:branch]       = PullRequestsData.read[pull_request_id][:head_branch]
+        req.params[:repository]   = PullRequestsData.read[pull_request_id][:head_url]
+        req.params[:base_branch]  = PullRequestsData.read[pull_request_id][:base_branch]
+      end
+      Logger.log("This is the branch and pull request tested - #{PullRequestsData.read[pull_request_id][:head_branch]} - #{PullRequestsData.read[pull_request_id][:head_url]} - #{PullRequestsData.read[pull_request_id][:base_branch]}")
       job_id
     rescue => e
       Logger.log('Error when starting job', e)
@@ -50,34 +67,42 @@ module Jenkins
     end
   end
 
-  def Jenkins.wait_on_job(job_id)
+  def Jenkins.wait_on_job(job_id, jenkins_job_name)
     config = ConfigFile.read
     while true
-      state = get_job_state(job_id)
+      state = Jenkins.get_job_state(job_id, jenkins_job_name)
       return state if !state.nil?
       sleep config[:jenkins_polling_interval_seconds]
     end
   end
 
-  def Jenkins.get_job_state(job_id)
+  def Jenkins.get_job_state(job_id, jenkins_job_name)
     begin
       config = ConfigFile.read
-      connection = Jenkins.new_connection("#{config[:jenkins_url]}/job/#{config[:jenkins_job_name]}/api/json", config, :use_json => true)
+      connection = Faraday.new(:url => "#{config[:jenkins_url]}/job/#{jenkins_job_name}/api/json") do |c|
+        c.use Faraday::Request::UrlEncoded
+        c.use FaradayMiddleware::FollowRedirects
+        c.use FaradayMiddleware::Mashify
+        c.use FaradayMiddleware::ParseJson
+        c.use Faraday::Adapter::NetHttp
+      end
+
+      if config.has_key?(:jenkins_login) && config.has_key?(:jenkins_password)
+        connection.basic_auth config[:jenkins_login], config[:jenkins_password]
+      end
 
       response = connection.get do |req|
         req.params[:depth] = 1
-        req.params[:tree] = 'builds[actions[parameters[name,value]],building,result,url]'
+        req.params[:tree] = 'builds[actions[parameters[name,value]],result,url]'
       end
 
       state = nil
       response.body[:builds].each do |build|
         begin
           if build[:actions][0][:parameters][2][:value] == job_id
-            if !build[:building]
-              state = {:status => 'success', :url => build[:url]} if build[:result] == 'SUCCESS'
-              state = {:status => 'failure', :url => build[:url]} if build[:result] == 'UNSTABLE'
-              state = {:status => 'failure', :url => build[:url]} if build[:result] == 'FAILURE'
-            end
+            state = {:status => 'success', :url => build[:url]} if build[:result] == 'SUCCESS'
+            state = {:status => 'failure', :url => build[:url]} if build[:result] == 'UNSTABLE'
+            state = {:status => 'failure', :url => build[:url]} if build[:result] == 'FAILURE'
           end
         rescue
         end
@@ -88,22 +113,5 @@ module Jenkins
       sleep 5
       retry
     end
-  end
-
-  def Jenkins.new_connection(url, config, opts = {})
-    connection = Faraday.new(:url => url) do |c|
-      c.use Faraday::Request::UrlEncoded
-      c.use FaradayMiddleware::FollowRedirects
-      c.use Faraday::Adapter::NetHttp
-      if opts[:use_json]
-        c.use FaradayMiddleware::Mashify
-        c.use FaradayMiddleware::ParseJson
-      end
-    end
-
-    if config.has_key?(:jenkins_login) && config.has_key?(:jenkins_password)
-      connection.basic_auth(config[:jenkins_login], config[:jenkins_password])
-    end
-    connection
   end
 end
