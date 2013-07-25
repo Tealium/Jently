0fd37d8b6b4a0c23d464198b5fd90e915b13ee22
JenRed12 <jennifer.frisk@tealium.com>
2013-07-24 17:14:46 -0700

    changes to core

--- lib/core.rb
+++ lib/core.rb
@@ -1,29 +1,33 @@
 module Core
-  def Core.test_pull_request(pull_request_id)
+  def Core.test_pull_request(pull_request_id, jenkins_job_name)
     begin
       config = ConfigFile.read
       pull_request = PullRequestsData.read[pull_request_id]
+      
 
+    if is_test_required
       if pull_request[:mergeable] == false
-        Github.set_pull_request_status(pull_request_id, {:status => 'failure', :description => 'Unmergeable pull request.'})
+        Github.set_pull_request_status(pull_request_id, {:status => 'failure', :description => 'Unmergeable pull request.'}, jenkins_job_name)
       end
 
       if pull_request[:mergeable] == true
-        Git.setup_testing_branch(pull_request)
-        Jenkins.wait_for_idle_executor
+        #Git.setup_testing_branch(pull_request)
 
         thr = Thread.new do
-          Github.set_pull_request_status(pull_request_id, {:status => 'pending', :description => 'Started work on pull request.'})
-          job_id = Jenkins.start_job(pull_request_id)
-          state = Jenkins.wait_on_job(job_id)
-          Github.set_pull_request_status(pull_request_id, state)
+          Github.set_pull_request_status(pull_request_id, {:status => 'pending', :description => 'Jankins has started work on pull request.'}, jenkins_job_name)
+          job_id = Jenkins.start_job(jenkins_job_name, pull_request_id)
+          state = Jenkins.wait_on_job(job_id, jenkins_job_name)
+          Github.set_pull_request_status(pull_request_id, state, job_id, jenkins_job_name)
         end
+        
+        Jenkins.wait_for_idle_executor
 
         timeout = thr.join(config[:jenkins_job_timeout_seconds]).nil?
-        Github.set_pull_request_status(pull_request_id, {:status => 'error', :description => 'Job timed out.'}) if timeout
+        Github.set_pull_request_status(pull_request_id, {:status => 'error', :description => 'Jenkins job timed out.'}) if timeout
       end
+    end
     rescue => e
-      Github.set_pull_request_status(pull_request_id, {:status => 'error', :description => 'An error has occurred. This pull request will be automatically rescheduled for testing.'})
+      Github.set_pull_request_status(pull_request_id, {:status => 'error', :description => 'An error has occurred.'})
       Logger.log('Error when testing pull request', e)
     end
   end
@@ -34,13 +38,12 @@ module Core
 
     open_pull_requests_ids.each do |pull_request_id|
       pull_request = Github.get_pull_request(pull_request_id)
-      if PullRequestsData.outdated_success_status?(pull_request)
-        Github.set_pull_request_status(pull_request[:id], {:status => 'success', :description => "This has been rescheduled for testing as the '#{pull_request[:base_branch]}' branch has been updated."})
-      end
-      PullRequestsData.update(pull_request)
+     # PullRequestsData.update(pull_request)
     end
 
-    pull_request_id_to_test = PullRequestsData.get_pull_request_id_to_test
-    test_pull_request(pull_request_id_to_test) if !pull_request_id_to_test.nil?
+    is_comment_valid, jenkins_jpb_name = Github.get_pull_request_comment(pull_request_id)
+    is_merged = pull_request[:merged]
+    is_test_required = !is_merged && is_comment_valid
+    test_pull_request(pull_request_id_to_test, jenkins_job_name) if is_test_required
   end
 end
